a38563951bfa3fded3b6296e31841c8dc63bd90e
Ricardo Casía <ricardocasiam@gmail.com>
2021-09-12 20:53:22 +0200

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StatsCounterComponent } from './stats-counter/stats-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    SidebarComponent,
+    StatsCounterComponent
   ],
   imports: [
     BrowserModule,
