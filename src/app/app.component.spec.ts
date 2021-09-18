d2e28e221e63a9a52b18bedac30c93cb3f221848
Ricardo Casía <ricardocasiam@gmail.com>
2021-09-19 01:12:41 +0200

    Edited title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'typ3'`, () => {
+  it(`should have as title 'typ3r'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('typ3');
+    expect(app.title).toEqual('typ3r');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('typ3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('typ3r app is running!');
   });
 });
